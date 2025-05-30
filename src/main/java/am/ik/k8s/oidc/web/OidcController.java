0dd606faca9e088eea978a4004f677dc0d25336d
Toshiaki Maki <makingx@gmail.com>
2025-05-31 00:26:49 +0900

    Refactor

--- src/main/java/am/ik/k8s/oidc/web/OidcController.java
+++ src/main/java/am/ik/k8s/oidc/web/OidcController.java
@@ -1,82 +1,36 @@
 package am.ik.k8s.oidc.web;
 
-import am.ik.k8s.oidc.KubernetesProps;
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.net.URI;
-import java.nio.charset.StandardCharsets;
+import am.ik.k8s.oidc.jwt.JwtProps;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import java.util.List;
 import java.util.Map;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.web.client.RestClientSsl;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.ResponseEntity;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestClient;
-import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 public class OidcController {
 
-	private final RestClient restClient;
+	private final JwtProps jwtProps;
 
-	private final Logger log = LoggerFactory.getLogger(getClass());
-
-	public OidcController(KubernetesProps props, RestClient.Builder restClientBuilder, RestClientSsl clientSsl) {
-		if (StringUtils.hasLength(props.clientBundleName())) {
-			log.info("Use clientBundle: {}", props.clientBundleName());
-			restClientBuilder.apply(clientSsl.fromBundle(props.clientBundleName()));
-		}
-		this.restClient = restClientBuilder.defaultHeaders(headers -> {
-			if (props.bearerToken() != null) {
-				try {
-					headers.setBearerAuth(props.bearerToken().getContentAsString(StandardCharsets.UTF_8));
-				}
-				catch (IOException e) {
-					throw new UncheckedIOException(e);
-				}
-			}
-		}).baseUrl(props.apiServerUrl()).defaultStatusHandler(status -> true, (request, response) -> {
-
-		}).build();
+	public OidcController(JwtProps jwtProps) {
+		this.jwtProps = jwtProps;
 	}
 
+	// https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_providers_create_oidc.html#manage-oidc-provider-prerequisites
 	@GetMapping(path = "/.well-known/openid-configuration")
-	public Map<String, Object> openidConfiguration(UriComponentsBuilder uriComponentsBuilder) {
-		Map<String, Object> configuration = getConfiguration();
-		String proxyJwksUri = uriComponentsBuilder.path("/openid/v1/jwks").build().toString();
-		configuration.put("jwks_uri", proxyJwksUri);
-		return configuration;
+	public Map<String, Object> openIdConfiguration(UriComponentsBuilder builder) {
+		return Map.of("issuer", builder.replacePath("").build().toString(), "jwks_uri",
+				builder.replacePath("openid/v1/jwks").build().toString(), "response_types_supported",
+				List.of("id_token"), "id_token_signing_alg_values_supported", List.of("RS256"),
+				"subject_types_supported", List.of("public"));
 	}
 
 	@GetMapping(path = "/openid/v1/jwks")
-	public Map<String, Object> jwks() {
-		Map<String, Object> configuration = getConfiguration();
-		String jwksUri = (String) configuration.get("jwks_uri");
-		return this.restClient.get()
-			.uri(URI.create(jwksUri).getPath())
-			.retrieve()
-			.body(new ParameterizedTypeReference<>() {
-			});
-	}
-
-	private Map<String, Object> getConfiguration() {
-		ResponseEntity<Map<String, Object>> response = this.restClient.get()
-			.uri("/.well-known/openid-configuration")
-			.retrieve()
-			.toEntity(new ParameterizedTypeReference<>() {
-			});
-		Map<String, Object> body = response.getBody();
-		if (!response.getStatusCode().is2xxSuccessful()) {
-			throw new ResponseStatusException(response.getStatusCode(), body != null ? body.toString() : "");
-		}
-		if (body == null) {
-			throw new IllegalStateException("Response body is null");
-		}
-		return body;
+	public Map<String, Object> tokenKeys() {
+		RSAKey key = new RSAKey.Builder(this.jwtProps.publicKey()).keyID(this.jwtProps.keyId()).build();
+		return new JWKSet(key).toJSONObject();
 	}
 
 }
--- src/test/java/am/ik/k8s/oidc/K8sOidcExposerApplicationTests.java
+++ src/test/java/am/ik/k8s/oidc/K8sOidcExposerApplicationTests.java
@@ -4,50 +4,22 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.TextNode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.web.client.RestClient;
-import org.testcontainers.containers.output.Slf4jLogConsumer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.k3s.K3sContainer;
-import org.testcontainers.utility.DockerImageName;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Testcontainers(disabledWithoutDocker = true)
 class K8sOidcExposerApplicationTests {
 
 	RestClient restClient;
 
-	@Container
-	static K3sContainer k3sContainer = new K3sContainer(DockerImageName.parse("rancher/k3s:v1.31.5-k3s1"))
-		.withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger("k3s")));
-
 	@LocalServerPort
 	int port;
-
-	@DynamicPropertySource
-	static void k3sProperties(DynamicPropertyRegistry registry) {
-		Cluster cluster = Cluster.fromConfigYaml(k3sContainer.getKubeConfigYaml());
-		registry.add("kubernetes.api-server-url", cluster::server);
-		registry.add("kubernetes.client-bundle-name", () -> "k3s");
-		registry.add("kubernetes.bearer-token", () -> "");
-		registry.add("spring.ssl.bundle.pem.k3s.truststore.certificate",
-				() -> "base64:" + cluster.certificateAuthorityData());
-		registry.add("spring.ssl.bundle.pem.k3s.keystore.certificate",
-				() -> "base64:" + cluster.clientCertificateData());
-		registry.add("spring.ssl.bundle.pem.k3s.keystore.private-key", () -> "base64:" + cluster.clientKeyData());
-	}
 
 	@BeforeEach
 	void setup(@Autowired RestClient.Builder restClientBuilder) {
@@ -63,8 +35,8 @@ class K8sOidcExposerApplicationTests {
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 		JsonNode body = response.getBody();
 		assertThat(body).isNotNull();
-		assertThat(body.get("issuer")).isEqualTo(new TextNode("https://kubernetes.default.svc.cluster.local"));
-		assertThat(body.get("jwks_uri")).isEqualTo(new TextNode("http://localhost:%d/openid/v1/jwks".formatted(port)));
+		assertThat(body.get("issuer")).isEqualTo(new TextNode("http://localhost:" + port));
+		assertThat(body.get("jwks_uri")).isEqualTo(new TextNode("http://localhost:" + port + "/openid/v1/jwks"));
 	}
 
 	@Test
