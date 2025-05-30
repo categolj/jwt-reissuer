325901d279119014d02f8de8f8a355ebfc7e9fda
Toshiaki Maki <makingx@gmail.com>
2025-05-31 01:11:04 +0900

    Change how to set default k8s api server

--- src/main/java/am/ik/k8s/oidc/config/KubernetesProps.java
+++ src/main/java/am/ik/k8s/oidc/config/KubernetesProps.java
@@ -1,10 +1,8 @@
 package am.ik.k8s.oidc.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.core.io.Resource;
 
 @ConfigurationProperties(prefix = "kubernetes")
-public record KubernetesProps(@DefaultValue("https://kubernetes.default.svc") String apiServerUrl, Resource bearerToken,
-		String clientBundleName) {
+public record KubernetesProps(String apiServerUrl, Resource bearerToken, String clientBundleName) {
 }
